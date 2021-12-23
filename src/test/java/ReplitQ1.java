b6c75ae3445249be5de987910135cc085dd285ff
mnvr00 <munevveroguz1@gmail.com>
2021-12-23 21:26:17 +0000

    first example

--- src/test/java/ReplitQ1.java
+++ src/test/java/ReplitQ1.java
@@ -1,3 +1,40 @@
 public class ReplitQ1 {
 
+    public static void main(String[] args) {
+
+        int[] x = {2,2};
+        System.out.println(has23(x));
+
+
+    }
+    public static boolean has23(int[] num) {
+        int dummy1 = 0;
+        int dummy2 = 0;
+
+        for (int j : num) {
+
+
+            if (j == 2) {
+                dummy1++;
+
+            } else if (j == 3) {
+                dummy2++;
+
+            }
+        }
+
+        return dummy1 == 2 || dummy2 == 2;
+
+
+
+
+
+
+
+    }
+
 }
+
+
+
+
