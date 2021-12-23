a21798fd1f4fc5757060cc9f0bf13cf97e9d9e51
Yasemin0955 <ymacit0955@gmail.com>
2021-12-23 22:36:36 +0000

    replit4 multidimensonal arrays

---
+++ src/test/java/Replit4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Replit4 {
+    public static void main(String[] args) {
+        int[][] values={
+                {8,20,50},
+                {12,30,75},
+
+        };
+
+
+
+
+        //DO NOT CHANGE
+        System.out.println(Arrays.deepToString(values));
+    }
+
+}
